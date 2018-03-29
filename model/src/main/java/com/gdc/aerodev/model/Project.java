bd8dfa104f070a6dded9a903708940fe9f3efadb
Yusupov Danil <danilka5318rus@gmail.com>
2018-03-29 15:49:58 +0300

    Created model entities

---
+++ model/src/main/java/com/gdc/aerodev/model/Project.java
@@ -0,0 +1,47 @@
+package com.gdc.aerodev.model;
+
+/**
+ * This entity can be created only by {@code User}. But {@code Cr} can review it. Also any project can be rated by
+ * {@code User} and {@code Cr} in future versions.
+ *
+ * @author Yusupov Danil
+ */
+public class Project {
+
+    private Long projectId;
+    private String projectName;
+    private Long projectOwner;
+    private ProjectType projectType;
+    private String projectDescription;
+
+    public Project() {
+    }
+
+    public Project(Long projectId, String projectName, Long projectOwner, ProjectType projectType, String projectDescription) {
+        this.projectId = projectId;
+        this.projectName = projectName;
+        this.projectOwner = projectOwner;
+        this.projectType = projectType;
+        this.projectDescription = projectDescription;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public Long getProjectOwner() {
+        return projectOwner;
+    }
+
+    public ProjectType getProjectType() {
+        return projectType;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+}
