84dff3e19bf1403f746eceeeffa57e3e8cb390d3
Yusupov Danil <danilka5318rus@gmail.com>
2018-03-30 18:15:48 +0300

    Added user update and try/catch on save()

--- service/src/main/java/com/gdc/aerodev/service/specific/UserService.java
+++ service/src/main/java/com/gdc/aerodev/service/specific/UserService.java
@@ -1,5 +1,6 @@
 package com.gdc.aerodev.service.specific;
 
+import com.gdc.aerodev.dao.exception.DaoException;
 import com.gdc.aerodev.dao.specific.UserDao;
 import com.gdc.aerodev.model.User;
 import com.gdc.aerodev.service.GenericService;
@@ -23,8 +24,32 @@ public class UserService extends GenericService{
         if (emailOwner != null){
             return "This email is already used by '" + emailOwner + "'.";
         }
-        Long id = dao.save(new User(userName, userPassword, userEmail));
-        return "Successful created user '" + userName + "' with id " + id + ".";
+        try {
+            Long id = dao.save(new User(userName, userPassword, userEmail));
+            return "Successful created user '" + userName + "' with id " + id + ".";
+        } catch (DaoException e){
+            return e.getMessage();
+        }
+    }
+
+    public String updateUser(Long userId, String userName, String userPassword, String userEmail, short userLevel){
+        User user = dao.getById(userId);
+        if (!userName.equals("")){
+            user.setUserName(userName);
+        }
+        if (!userPassword.equals("")){
+            user.setUserPassword(userPassword);
+        }
+        if (!userEmail.equals("")){
+            user.setUserEmail(userEmail);
+        }
+        user.setUserLevel(userLevel);
+        try {
+            dao.save(user);
+        } catch (DaoException e){
+            return e.getMessage();
+        }
+        return "Successfully updated user '" + userName + "'.";
     }
 
 }
