34265e2a67386d03fec030fc6e8f55f79b7f52dd
Yusupov Danil <danilka5318rus@gmail.com>
2018-04-06 18:28:42 +0300

    Performed module built app with auto migration

--- web/src/main/java/com/gdc/aerodev/web/WebServiceApp.java
+++ web/src/main/java/com/gdc/aerodev/web/WebServiceApp.java
@@ -1,16 +1,18 @@
 package com.gdc.aerodev.web;
 
+import com.gdc.aerodev.service.ServiceApp;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class AerodevApplication {
+@ComponentScan(basePackages = "com.gdc.aerodev")
+public class WebServiceApp {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(AerodevApplication.class)
+        new SpringApplicationBuilder(ServiceApp.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
     }
-
 }
