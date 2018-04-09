1ef978bac84a7a147f48cfe84d28a7182f0cb5a6
Yusupov Danil <danilka5318rus@gmail.com>
2018-04-09 12:58:29 +0300

    Added important comment

--- web/src/main/java/com/gdc/aerodev/web/WebServiceApp.java
+++ web/src/main/java/com/gdc/aerodev/web/WebServiceApp.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * This application works only if PostrgeSQL server started with configured 'application.yml' flyway properties
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = "com.gdc.aerodev")
 public class WebServiceApp {
