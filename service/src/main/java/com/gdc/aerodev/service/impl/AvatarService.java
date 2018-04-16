ae41bdb47ab4f5aca30e87dce72f81b05979b3f7
Yusupov Danil <danilka5318rus@gmail.com>
2018-04-16 18:42:43 +0300

    Realized uploading/downloading avatar and bind with views

--- service/src/main/java/com/gdc/aerodev/service/impl/AvatarService.java
+++ service/src/main/java/com/gdc/aerodev/service/impl/AvatarService.java
@@ -1,31 +1,50 @@
 package com.gdc.aerodev.service.impl;
 
 import com.gdc.aerodev.dao.postgres.PostgresAvatarDao;
-import com.gdc.aerodev.dao.postgres.PostgresUserDao;
 import com.gdc.aerodev.model.Avatar;
 import com.gdc.aerodev.service.GenericAvatarService;
 import com.gdc.aerodev.service.logging.LoggingService;
+import org.mockito.internal.matchers.Null;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AvatarService implements GenericAvatarService, LoggingService {
 
     private final PostgresAvatarDao avDao;
-    private final PostgresUserDao usrDao;
 
-    public AvatarService(PostgresAvatarDao avDao, PostgresUserDao usrDao) {
+    public AvatarService(PostgresAvatarDao avDao) {
         this.avDao = avDao;
-        this.usrDao = usrDao;
+    }
+
+    /**
+     * Checks existence of avatar by {@code User} id
+     * @param id of user
+     * @return (0) true if exists or
+     *         (1) false if not exists
+     */
+    public boolean isExistent(Long id){
+        return avDao.getById(id) != null;
     }
 
 
-    @Override
-    public Long uploadAvatar(Avatar avatar) {
-        return null;
-    }
-
+    /**
+     * Selects user's avatar from DB
+     * @param id of avatar's owner
+     * @return (0) {@code Avatar} or
+     *         (1) {@code null}
+     */
     @Override
     public Avatar getAvatar(Long id) {
+        return avDao.getById(id);
+    }
 
-        return null;
+    public Long uploadAvatar(Long userId, byte[] bytes, String contentType) {
+        try {
+            Long avatarId = avDao.getById(userId).getAvatarId();
+            return avDao.save(new Avatar(avatarId, userId, bytes, contentType));
+        } catch (NullPointerException e){
+            return avDao.save(new Avatar(userId, bytes, contentType));
+        }
     }
 }
 
