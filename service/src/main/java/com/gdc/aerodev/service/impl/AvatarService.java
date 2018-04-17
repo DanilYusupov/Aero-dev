a7dfd8daa8c26d3030f2dee508cc588e195df630
Danil Yusupov <danilka5318@yandex.ru>
2018-04-17 09:53:01 +0300

    Realized default avatar logic without m/f

--- service/src/main/java/com/gdc/aerodev/service/impl/AvatarService.java
+++ service/src/main/java/com/gdc/aerodev/service/impl/AvatarService.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 public class AvatarService implements GenericAvatarService, LoggingService {
 
     private final PostgresAvatarDao avDao;
+    private final Long DEFAULT_MAN_AVATAR = 1L;
+    private final Long DEFAULT_WOMAN_AVATAR = 2L;
 
     public AvatarService(PostgresAvatarDao avDao) {
         this.avDao = avDao;
@@ -18,28 +20,30 @@ public class AvatarService implements GenericAvatarService, LoggingService {
 
     /**
      * Checks existence of avatar by {@code User} id
+     *
      * @param id of user
      * @return (0) true if exists or
-     *         (1) false if not exists
+     * (1) false if not exists
      */
-    public boolean isExistent(Long id){
+    public boolean isExistent(Long id) {
         return avDao.getById(id) != null;
     }
 
 
     /**
      * Selects user's avatar from DB
+     *
      * @param id of avatar's owner
      * @return (0) {@code Avatar} or
-     *         (1) {@code null}
+     * (1) {@code null}
      */
     @Override
     public Avatar getAvatar(Long id) {
-        try{
-            return avDao.getById(id);
-        } catch (NullPointerException e){
-            return null;
-            //FIXME: return default avatar
+        Avatar avatar = avDao.getById(id);
+        if (avatar == null) {
+            return avDao.getById(DEFAULT_MAN_AVATAR);
+        } else {
+            return avatar;
         }
     }
 
@@ -47,7 +51,7 @@ public class AvatarService implements GenericAvatarService, LoggingService {
         try {
             Long avatarId = avDao.getById(userId).getAvatarId();
             return avDao.save(new Avatar(avatarId, userId, bytes, contentType));
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             return avDao.save(new Avatar(userId, bytes, contentType));
         }
     }
